505844c42bbfec5151bb273c25445f4c7ad8cf62
levieraf <levieraf@gmail.com>
2013-05-16 18:36:01 -0500

    Update index.php
    could be better like that for show any error (Y)

--- example/index.php
+++ example/index.php
@@ -7,9 +7,13 @@ include_once('../src/FullContact.php');
 //get your api key here:  http://fullcontact.com/getkey
 $fullcontact = new FullContactAPI($apikey);
 
-//do a lookup
-$result = $fullcontact->doLookup('bart@fullcontact.com');
 
+//do a lookup
+try{
+$result = $fullcontact->doLookup('bart@fullcontact.com');
+}catch(Exception $e){
+  $result = $e->getMessage();
+}
 //dump our results
 echo "<br/>----------------<br/><pre>";
 print_r($result);
